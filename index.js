51b470b61b87e5b451317719e80c44378961bf3f
Shashank Taliwal <shashanktaliwal07@gmail.com>
2021-08-05 17:58:29 +0530

    All fucntionalities completed in due time

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.get('/',(req,res)=>{
     res.render('home',{
-        fileData:'Your data appears here'
+        fileData:'Your data appears here',
+        maxWords:'null',
+        maxCount:0
     });
 });
 
@@ -17,8 +19,31 @@ app.post('/',(req,res)=>{
     const bookName=req.body.book;
     const data=fs.readFileSync('HP.txt','utf-8');
     // console.log(data);
+    var allWords = data.split(/\b/);
+var wordCountList = {};
+
+allWords.forEach(function(word){
+  if(word !== " "){
+    if(!wordCountList.hasOwnProperty(word)){
+      wordCountList[word] = {word: word, count:0};
+    }
+    wordCountList[word].count++;
+  }
+})
+
+
+var maxCountWord = {count:0};
+for(var propName in wordCountList){
+  var currentWord = wordCountList[propName];
+  if(maxCountWord.count<currentWord.count){
+    maxCountWord = currentWord;
+  }
+}
+console.log(maxCountWord);
     res.render('home',{
-        fileData:data
+        fileData:data,
+        maxWords:maxCountWord.word,
+        maxCount:maxCountWord.count
     });
 });
 app.listen(3000,()=>{
